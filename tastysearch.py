8cbc98192c0099e7453c40c1728b69753d7c7109
Lord and Master of All Things Big and Small <amphy.kit@gmail.com>
2019-12-09 16:52:39 -0600

    prelim results page with fake results

--- tastysearch.py
+++ tastysearch.py
@@ -6,6 +6,33 @@ import json
 
 app = Flask(__name__)
 
+test = [{
+	"title": "Paper 1",
+	"abstract": "Fake Abstract 1",
+	"author": "John B. Doe",
+	"keywords": ["paper", "abstract", "test"]
+}, {
+	"title": "Paper 2",
+	"abstract": "Fake Abstract 2",
+	"author": "John B. Doe II",
+	"keywords": ["paper", "abstract", "test"]
+}, {
+	"title": "Paper 3",
+	"abstract": "Fake Abstract 3",
+	"author": "Jane A. Doe",
+	"keywords": ["paper", "abstract", "test", "information retrieval"]
+}]
+
 @app.route('/')
 def hello(name=None):
     return render_template('index.html', name=name)
+
+@app.route('/results', methods=['GET'])
+def results():
+
+	pretty_query = request.args.get('query')
+	pretty_query = pretty_query[1:-1]
+
+	results = test
+
+	return render_template('results.html', results=results, query=pretty_query)
